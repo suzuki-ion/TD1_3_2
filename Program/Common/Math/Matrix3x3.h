10436d1c452abde4ba33a21e35270540b177a420
ミンツ 鈴木 <ionnnn.s69@gmail.com>
2025-03-19 12:07:22 +0900

    Matrix3x3のコンストラクタが不足してたので追加

--- Program/Common/Math/Matrix3x3.h
+++ Program/Common/Math/Matrix3x3.h
@@ -3,6 +3,10 @@
 /// @brief 3x3行列
 struct Matrix3x3 final {
     Matrix3x3() = default;
+    Matrix3x3(float m00, float m01, float m02,
+        float m10, float m11, float m12,
+        float m20, float m21, float m22);
+    Matrix3x3(float matrix[9]);
     Matrix3x3(float matrix[3][3]);
     Matrix3x3(const Matrix3x3 &matrix);
     Matrix3x3 operator=(const Matrix3x3 &matrix) const;
--- Program/Common/Math/Source/Matrix3x3.cpp
+++ Program/Common/Math/Source/Matrix3x3.cpp
@@ -1,5 +1,23 @@
 #include <Common/Math/Matrix3x3.h>
 
+Matrix3x3::Matrix3x3(float m00, float m01, float m02, float m10, float m11, float m12, float m20, float m21, float m22) {
+    m[0][0] = m00;
+    m[0][1] = m01;
+    m[0][2] = m02;
+    m[1][0] = m10;
+    m[1][1] = m11;
+    m[1][2] = m12;
+    m[2][0] = m20;
+    m[2][1] = m21;
+    m[2][2] = m22;
+}
+
+Matrix3x3::Matrix3x3(float matrix[9]) {
+    for (int i = 0; i < 9; i++) {
+        m[i / 3][i % 3] = matrix[i];
+    }
+}
+
 Matrix3x3::Matrix3x3(float matrix[3][3]) {
     for (int row = 0; row < 3; row++) {
         for (int column = 0; column < 3; column++) {
